8a527de24353b78d037fa4a92315bdfb3f081e56
TanishJ05 <tanishj0522@gmail.com>
2026-05-29 15:06:57 +0530

    updated

---
+++ 10/Problem3.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+void RangeSum(int iStart, int iEnd)
+{
+    int iCnt = 0;
+    int iSum = 0;
+    if(iStart > iEnd)
+    {
+        printf("Invalid Range\n");
+        return;
+    }
+    for(iCnt = iStart; iCnt <= iEnd; iCnt++)
+    {
+        iSum = iSum + iCnt;
+    }
+    printf("Sum of numbers in the range is : %d",iSum);
+    printf("\n");
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter the starting point : ");
+    scanf("%d",&iValue1);
+    printf("Enter the ending point : ");
+    scanf("%d",&iValue2);
+
+    RangeSum(iValue1, iValue2);
+    return 0;
+}
+// Time complexity - O(N)
